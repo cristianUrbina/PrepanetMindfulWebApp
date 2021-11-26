d0545b9a9a1850e5413c1a75101eb13baea2a54c
cristianUrbina <A01412249@itesm.mx>
2021-11-25 23:17:15 -0600

    Working with final database

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,7 @@ var admin = require("firebase-admin");
 //const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 //const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-const serviceAccount = require("./prepanetmindfuldb-firebase-adminsdk-7iiyh-fb8ac1e56f.json");
+const serviceAccount = require("./prepanetmindful-firebase-adminsdk-7n6t5-466288466e.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
